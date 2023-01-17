b346e095960507a56d09b3ebfe5d4427b37291fa
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-01-17 20:22:32 +0000

    upload 001

---
+++ components/components/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    let colorcode = {
+        period1: "#5d9e57",
+        period2: "#3264cf",
+        period3: "#32ccf4"
+    };
+    let timeline = new PRESTimeline($("#this-timeline"), colorcode);
+});
+
