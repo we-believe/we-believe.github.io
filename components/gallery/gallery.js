92bdb0d4e95c07d49c136ddaee2b3b58845b06fa
ntluiz <77010431+ntluiz@users.noreply.github.com>
2023-01-25 16:53:00 +0000

    002

--- components/gallery/gallery.js
+++ components/gallery/gallery.js
@@ -1,6 +1,18 @@
+console.log(
+    document.querySelectorAll(".carousel-item.h-100.w-100.active")
+)
+var prev = $("button.carousel-control-prev");
+
+var menuimg = $("div.col-2 div.container img");
+
+var slide = $("div.carousel-item");
+
+var child = $("div.active").child(0);
+
 $(document).ready(function () {
     $('#galleryimg1').click(function () {
-        alert('teste');
-        // ocultaForm(); pode chamar aqui
+        $(document).ready(function () {
+            $("#slide1").addClass("active");
+        });
     });
 });
