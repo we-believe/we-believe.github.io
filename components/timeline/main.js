cc434dd8813cb13fd1b60208a79898df3d2d6ba9
ntluiz <77010431+ntluiz@users.noreply.github.com>
2025-10-29 14:17:42 -0300

    Delete components directory

--- components/timeline/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    let colorcode = {
-        period1: "#5d9e57",
-        period2: "#3264cf",
-        period3: "#32ccf4"
-    };
-    let timeline = new PRESTimeline($("#this-timeline"), colorcode);
-});
-
